9bd828b17142f44417891219ef7b817aad5745fe
eugenioclrc <eugenioclrc@gmail.com>
2022-10-30 20:31:45 -0300

    desc matt

--- src/routes/tracks/eko2022/trickster/+page.svelte
+++ src/routes/tracks/eko2022/trickster/+page.svelte
@@ -6,7 +6,7 @@
 <PageChallenge challenge={$ekoLevels[2]}>
   <slot slot="challengeTitle">{$ekoLevels[2].name}</slot>
   <slot slot="content">
-    <p class="mt-2 mb-4 text-slate-700 text-lg">
+    <p class="text-lg">
       We might have spotted a honeypot... Can you manage to obtain the real jackpot?.<br />
       Hacking casino slot machines is considered illegal.
       <br /><br />
@@ -15,6 +15,14 @@
         jackpot.sol
       </a>
     </p>
+    <p>
+      <small>Challenge author: 
+        <a href="https://twitter.com/mattaereal" target="_blank" rel="noreferrer" class="link-hover link-neutral">mattaereal</a>: 
+        Hacker. Security researcher. Currently working in Ethereum.
+      </small>
+    </p>
+    
+    
   </slot>
 
   <slot slot="code">
