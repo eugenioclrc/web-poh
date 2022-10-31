47f01feac6990878a81be9919c23eaeb66e78065
eugenioclrc <eugenioclrc@gmail.com>
2022-10-31 16:14:54 -0300

    tw authors links

--- src/routes/tracks/eko2022/+page.svelte
+++ src/routes/tracks/eko2022/+page.svelte
@@ -14,7 +14,16 @@ import ekoLevels from "$lib/levels-eko";
       </h2>
       <span class="text-sm">
         A collection of challenges made for the  <a href="https://www.ekoparty.org/" target="_blank" rel="noreferrer">EKO party</a> submited by some gigabrain hackers;
-        @bronicle, @adriro, @nicobevi.eth, @tnth, @matta and 0x4non...<br />
+        <a href="https://twitter.com/Cryptonicle1" target="_blank" rel="noreferrer" class="link-hover link-neutral">@Br0niclΞ</a>,
+        <a href="https://twitter.com/nicobevi_eth" target="_blank" rel="noreferrer" class="link-hover link-neutral">@nicobevi.eth</a>,
+        <a href="https://twitter.com/mattaereal" target="_blank" rel="noreferrer" class="link-hover link-neutral">@matta</a>,
+        <a href="https://twitter.com/tinchoabbate" target="_blank" rel="noreferrer" class="link-hover link-neutral">@tinchoabbate</a>,
+        <a href="https://twitter.com/adrianromero" target="_blank" rel="noreferrer" class="link-hover link-neutral">@adriro</a>,
+        <a href="https://twitter.com/AugustitoQ" target="_blank" rel="noreferrer" class="link-hover link-neutral">@bengalaQ</a>,
+        <a href="https://linktr.ee/chiin.eth" target="_blank" rel="noreferrer" class="link-hover link-neutral">@chiin</a>,
+        <a href="https://twitter.com/victor93389091" target="_blank" rel="noreferrer" class="link-hover link-neutral">@Rotciv</a>
+        and 
+        <a href="https://twitter.com/eugenioclrc" target="_blank" rel="noreferrer" class="link-hover link-neutral">@0x4non</a>...<br />
         <b>Challenges will be posted the 02/11/2022.</b>
       </span>
     </div>
