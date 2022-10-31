2528df722f0e2117fbe896b8f3ce781abfde36b5
eugenioclrc <eugenioclrc@gmail.com>
2022-10-31 09:56:43 -0300

    phoenixtto por thething

--- src/routes/tracks/eko2022/phoenixtto/+page.svelte
+++ src/routes/tracks/eko2022/phoenixtto/+page.svelte
@@ -7,11 +7,14 @@
   <slot slot="challengeTitle">{$ekoLevels[8].name}</slot>
   <slot slot="content">
     <p class="text-lg">
-      At an Antarctica research site, the discovery of an alien form contract leads you to a confrontation. Take ownership of <em>the think</em> or be assimilated.
+      Within the world of crossovers there is a special one, where the universes of pokemon, harry potter and solidity intertwine. In 
+      this crossover a mix creature is created between dumbledore's phoenix, a wild ditto and since we are in the solidity universe 
+      this creature is a contract. We have called it Phoenixtto and it has two important abilities, that of being reborn from it's 
+      ashes after its destruction and that of copying the behavior of another bytecode Try to capture the Phoenixtto, if you can...
       <br /><br />
       <br />
       Source: <a href="https://github.com/Proof-Of-Hack-Protocol/challenges/blob/main/src" target="_blank" rel="noreferrer" class="link-hover link-primary">
-        thething.sol
+        phoenixtto.sol
       </a>
     </p>
     <p>
