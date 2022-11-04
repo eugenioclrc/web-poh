2ef4c7fabdb5fc5ee9ae4cf18f8734611dd9088f
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-11-04 14:33:13 -0300

    refactor

---
+++ src/lib/NameOrAddress.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getEnsName } from "$utils/ens";
+	import { onMount } from "svelte";
+
+  export let address;
+
+  let name;
+  onMount(async () => {
+    name = await getEnsName(address);
+  });
+</script>
+
+<span>
+  {#if name}
+    {name}
+  {:else if address} 
+    {address.slice(0,6)}... {address.slice(-4)}
+  {:else} 
+    asd
+  {/if}
+</span>
